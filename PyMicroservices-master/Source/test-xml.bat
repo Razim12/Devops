41c456a6dcee2c888a03c951a985bcd1e7f5434c
ABDUL QAYOOM RAZIM <35489549+Razim12@users.noreply.github.com>
2019-04-07 12:44:07 +0530

    Add files via upload

---
+++ PyMicroservices-master/Source/test-xml.bat
@@ -0,0 +1,11 @@
+REM SET PATH=%PATH%;c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32;c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\Scripts
+del C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master\Source\python_tests_xml\*.*
+del C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master\Source\coverage.xml
+cd C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master\Source
+REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python runtests.py
+REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage run -m pytest && coverage xml -o coverage.xml
+REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage html -d coverage
+python runtests.py
+python -m coverage run -m pytest && coverage xml -o coverage.xml
+copy C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master\Source\python_tests_xml\*.* C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master\python_tests_xml
+copy C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master\Source\coverage.xml C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master
