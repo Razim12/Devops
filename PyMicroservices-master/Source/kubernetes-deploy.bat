9797bece421d1ebde137b121322dd479f0d2e561
ABDUL QAYOOM RAZIM <35489549+Razim12@users.noreply.github.com>
2019-04-14 02:05:37 +0530

    Update kubernetes-deploy.bat

--- PyMicroservices-master/Source/kubernetes-deploy.bat
+++ PyMicroservices-master/Source/kubernetes-deploy.bat
@@ -1,5 +1,5 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-SET BUILD_NUMBER=53
-SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
-cd c:\skillvelocity\courses\devops\PyMicroservices\Source
+REM SET BUILD_NUMBER=53
+SET IMAGE_NAME=gcr.io/RSdevops/rsmicroimage:%BUILD_NUMBER%
+cd C:\Users\RAZIM\Downloads\devops-master\devops-master\PyMicroservices-master\PyMicroservices-master\Source
 kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
