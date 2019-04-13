fab63adacf490ba368dac1f8b09351f898a7e416
ABDUL QAYOOM RAZIM <35489549+Razim12@users.noreply.github.com>
2019-04-14 02:03:18 +0530

    Update pre-build-config.bat

--- PyMicroservices-master/Source/pre-build-config.bat
+++ PyMicroservices-master/Source/pre-build-config.bat
@@ -1,2 +1,2 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-gcloud auth activate-service-account jenkins@handle-books.iam.gserviceaccount.com --key-file=c:\Users\nvmur\Downloads\handle-books-2a419c24f2ac.json --project=handle-books
+gcloud auth activate-service-account rsservice@rsdevops-237520.iam.gserviceaccount.com --key-file=C:\Users\RAZIM\Downloads\rsdevops-237520-26ff4a2316fc
